e536beecc24ab3c3ce2dc1e875de831cb250da41
anonfedora <musaeleazar090@gmail.com>
2024-04-09 11:10:08 +0100

    authService fix 03

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
-import { UserModule } from './user/user.module';
+import "dotenv/config";
+/*import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { CartModule } from './cart/cart.module';
-//import { AuthService } from './auth/auth.service';
+import { CartModule } from './cart/cart.module';*/
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO_URI), 
-    ProductModule, UserModule, AuthModule, CartModule],
+    ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
