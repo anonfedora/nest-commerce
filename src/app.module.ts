f0dc35784a5d7f79cae510d84487b8f9d3a39b2c
anonfedora <musaeleazar090@gmail.com>
2024-04-09 13:24:11 +0100

    MongooseServerSelectionError: connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import "dotenv/config";
-/*import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { CartModule } from './cart/cart.module';*/
-import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO_URI), 
-    ProductModule, DatabaseModule],
+    ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
