9ec9f9a96ed12c78252e3f8270bb0239ffaf470d
anonfedora <musaeleazar090@gmail.com>
2024-04-09 12:23:37 +0100

    authService fix 05 - uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,18 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { ProductModule } from "./product/product.module";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 import "dotenv/config";
-import { DatabaseModule } from "./database/database.module";
+/*import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';*/
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-    imports: [
-        MongooseModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: async (config: ConfigService) => ({
-                uri: config.get<string>("MONGO_URI") // Loaded from .ENV
-            })
-        }),
-        ProductModule,
-        DatabaseModule
-    ],
-    controllers: [AppController],
-    providers: [AppService]
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), 
+    ProductModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
