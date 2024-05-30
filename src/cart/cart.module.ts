16d194201768d4e93e73450affeb2582d525fcc7
anonfedora <musaeleazar090@gmail.com>
2024-05-30 16:47:49 +0100

    Cart

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,14 +1,9 @@
-import { Module } from "@nestjs/common";
-import { CartService } from "./cart.service";
-import { CartController } from "./cart.controller";
-import { CartSchema } from "./schemas/cart.schema";
-import { MongooseModule } from "@nestjs/mongoose";
+import { Module } from '@nestjs/common';
+import { CartService } from './cart.service';
+import { CartController } from './cart.controller';
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([{ name: "Cart", schema: CartSchema }])
-    ],
-    providers: [CartService],
-    controllers: [CartController]
+  providers: [CartService],
+  controllers: [CartController]
 })
 export class CartModule {}
