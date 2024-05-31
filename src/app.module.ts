132d6270fb18a071aa28ec225406c9150034ab96
anonfedora <musaeleazar090@gmail.com>
2024-05-31 11:58:35 +0100

    Cart

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
+import { CartModule } from './cart/cart.module';
 import "dotenv/config";
 
 @Module({
