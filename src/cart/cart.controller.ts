0584586caebc9572bd19f0e4e48bfa9906eaece6
anonfedora <musaeleazar090@gmail.com>
2024-06-17 19:03:51 +0100

    Cart controller

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,34 +1,57 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CartService } from './cart.service';
-import { CreateCartDto } from './dto/create-cart.dto';
-import { UpdateCartDto } from './dto/update-cart.dto';
+import {
+    Controller,
+    Post,
+    Body,
+    Request,
+    UseGuards,
+    Delete,
+    NotFoundException,
+    Param
+} from "@nestjs/common";
 
-@Controller('cart')
+import { CartService } from "./cart.service";
+import { CreateCartDto } from "./dto/create-cart.dto";
+import { UpdateCartDto } from "./dto/update-cart.dto";
+import { Roles } from "src/auth/decorators/roles.decorator";
+import { Role } from "src/auth/enums/role.enum";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { RolesGuard } from "src/auth/guards/roles.guard";
+import { CartService } from "./cart.service";
+import { ItemDTO } from "./dtos/item.dto";
+
+@Controller("cart")
 export class CartController {
-  constructor(private readonly cartService: CartService) {}
+    constructor(private readonly cartService: CartService) {}
 
-  @Post()
-  create(@Body() createCartDto: CreateCartDto) {
-    return this.cartService.create(createCartDto);
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
+    @Post("/")
+    async addItemToCart(@Request() req, @Body() itemDTO: ItemDTO) {
+        const userId = req.user.userId;
+        const cart = await this.cartService.addItemToCart(userId, itemDTO);
+
+        return cart;
+    }
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
+    @Delete("/")
+    removeItemFromCart(@Request() req, @Body() { productId }) {
+        const userId = req.user.userId;
+        const cart = await this.cartService.removeItemFromCart(
+            userId,
+            productId
+        );
+        if (!cart) throw new.NotFoundException("Product Not found");
+        return cart;
+    }
+@UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.User)
+  @Delete('/:id')
+  async deleteCart(@Param('id') userId: string) {
+    const cart = await this.cartService.deleteCart(userId);
+    if (!cart) throw new NotFoundException('Cart does not exist');
+    return cart;
   }
 
-  @Get()
-  findAll() {
-    return this.cartService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.cartService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
-    return this.cartService.update(+id, updateCartDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.cartService.remove(+id);
-  }
 }
