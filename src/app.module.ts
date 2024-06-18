c022827dfc8eab6740817032ff6a5160518f4222
anonfedora <musaeleazar090@gmail.com>
2024-06-18 01:51:28 +0100

    Refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
-import { CartModule } from './cart/cart.module';
-import { CartModule } from './cart/cart.module';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ProductModule } from "./product/product.module";
+import { CartModule } from "./cart/cart.module";
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 import "dotenv/config";
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), 
-    ProductModule, CartModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot(process.env.MONGO_URI),
+        ProductModule,
+        CartModule, UserModule, AuthModule
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -16,7 +16,6 @@ import { Roles } from "src/auth/decorators/roles.decorator";
 import { Role } from "src/auth/enums/role.enum";
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import { RolesGuard } from "src/auth/guards/roles.guard";
-import { CartService } from "./cart.service";
 import { ItemDTO } from "./dtos/item.dto";
 
 @ApiTags("cart")
