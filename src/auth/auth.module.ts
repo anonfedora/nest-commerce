c6a21f4d9652a1a725e391be2fa8aea512f03c5b
anonfedora <musaeleazar090@gmail.com>
2024-06-18 02:31:11 +0100

    Refactoring

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,18 +2,17 @@ import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { UserService } from "../user/user.service";
 import { AuthController } from "./auth.controller";
-import { UserModule } from 'src/user/user.module';
+import { UserModule } from "src/user/user.module";
 import { LocalStrategy } from "./strategies/local.strategy";
 import { JwtStrategy } from "./strategies/jwt.strategy";
-import { UserModule } from "src/user/user.module";
 import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
 import "dotenv/config";
 
 @Module({
     imports: [
-      UserModule,
-      PassportModule,
+        UserModule,
+        PassportModule,
         JwtModule.register({
             secret: process.env.JWT_SECRET,
             signOptions: { expiresIn: "3600s" }
